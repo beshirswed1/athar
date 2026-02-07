e9bc166b4a7c5e5de15befc4dde107a77a22daa7
beshirswed1 <beshirswed07@gmail.com>
2026-02-08 00:53:51 +0300

    new lock

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,46 @@ import './globals.css';
 import { cairo } from '@/lib/font';
 import Header from '@/components/Header';
 import Providers from './providers';
+import { icons } from 'lucide-react';
 
 export const metadata = {
   title: 'ATHAR-أثر',
   description: 'منصة لإدارة مكتبتك الشخصية من الكتب الإسلامية والتاريخية',
+  keywords: 'مكتبة رقمية, كتب إسلامية, كتب تاريخية, إدارة القراءة, اكتشاف الكتب',
+  authors: [{ name: 'Beshir_Swed', url: 'https://github.com/beshirswed1' }],
+  opensGraph: {
+    title: 'ATHAR-أثر',
+    description: 'منصة لإدارة مكتبتك الشخصية من الكتب الإسلامية والتاريخية',
+    url: 'https://athar-one.vercel.app/',
+    siteName: 'ATHAR-أثر',
+    images: [
+      {
+        url: '/app/favicon.ico',
+        width: 1200,
+        height: 630,
+        alt: 'ATHAR-أثر - منصة لإدارة مكتبتك الشخصية'
+      }
+    ],
+    locale: 'ar_AR',
+    type: 'website'
+
+
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+    other: {
+      rel: 'icon',
+      url: '/favicon.ico',
+    },
+  },
+
+
+
+
+
+
+
 };
 
 export default function RootLayout({ children }) {
